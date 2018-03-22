80360293fa96898674991c383fe179266fb64f55
Neo Yiu <neo.yiu@rakuten.com>
2018-03-22 00:23:54 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// main app
+import App from './containers/App';
+import { Provider } from 'react-redux';
+// main routes
+import AppRoutes from './routes';
+import store from './store';
+ReactDOM.render(
+ <Provider store={store}>
+ <App />
+ </Provider>,
+ document.getElementById('app')
+)
